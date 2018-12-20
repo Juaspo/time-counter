c6bc6912060f2240772e226f7046104b4b4b3962
ezasaju <ezasaju@SE-00011408.ericsson.se>
2018-12-20 11:08:37 +0100

    terminal argument pass
    Added passing and handling of arguments from shell

--- src/gui/gui_main.py
+++ src/gui/gui_main.py
@@ -4,20 +4,22 @@ Created on 23 nov. 2018
 @author: ezasaju
 '''
 import time
+import sys, getopt
 
 from tkinter import *
 from tkinter import messagebox
 
 import main.main_program as mp
 import main.computer_control as cc
+from _overlapped import NULL
 
 
 #import tkinter
 #from tkinter import Button
 #from tkinter.ttk import Frame
 
-if __name__ == '__main__':
-    pass
+autostart = False
+take_timestamp = False
 
 
 top = Tk()
@@ -27,7 +29,7 @@ frame = Frame(top)
 frame.pack()
 
 
-mp.initiate_parameters()
+
 
 
 def quit():
@@ -45,14 +47,17 @@ def quit():
     #msg = messagebox.showinfo("Hi", "Hello World!")
 
 
-def run_clocking():
+def run_clocking(msg = None):
+    if msg is None:
+        msg = "user"
+        
     if(mp.is_working()):
-        mp.end_clocking("user")
+        mp.end_clocking(msg)
         start_btn.config(text = "Start")
         txt = mp.time_value_content()
         label.config(text = txt, bg="#e00")
     else:
-        mp.begin_clocking("user")
+        mp.begin_clocking(msg)
         start_btn.config(text = "Stop")
         txt = mp.time_value_content()
         label.config(text = txt, bg="#0e0")
@@ -60,7 +65,7 @@ def run_clocking():
     time.sleep(0.05)
     
     
-def timestamp():
+def timestamp(msg = None):
     action_type = 3
     
     #if in running mode change action to "Running"
@@ -68,9 +73,12 @@ def timestamp():
         action_type = 2
         
     print ("Timestamp")
-    input = text_entry.get()
-    if input =="":
-        input = "Timestamp"
+    if msg is None:
+        input = text_entry.get()
+        if input =="":
+            input = "Timestamp"
+    else:
+        input = msg
     mp.stamp_time(action_type, False, input)
     
     
@@ -84,11 +92,10 @@ def debugging_stuff():
     print ("changing date")
     txt = mp.change_Date()
     label.config(text = txt)
-
+    
+    
 label = Label(frame, text="Welcome!", fg="black", font="Verdana 30 bold") 
 label.pack() 
-
-
 
 start_btn = Button(frame, text = "Start", font="Verdana 20 bold", width = 10, height = 5, command = run_clocking)
 start_btn.pack()
@@ -115,5 +122,40 @@ exit_btn = Button(frame2, text="Exit", width = 15, command = quit)
 exit_btn.grid(row = 1, column = 1)
 #exit_btn.pack()
 
+
+mp.initiate_parameters()
+    
+def main(argv):
+    global take_timestamp
+    global autostart
+    
+    try:
+        opts, args = getopt.getopt(argv, "hs:t:", ["help"])
+    except getopt.GetoptError:
+        print("Wrong input try -h for help")
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        print("arg:", arg)
+        if opt in ("-h", "--help"):
+            print("\nTimeCounter help screen\n")
+            print("-h\tfor this help text\n-s\tfor auto start\n-t\tfor timestamp")
+            sys.exit()
+        elif opt == "-t":
+            timestamp(arg)
+            take_timestamp = True
+        elif opt == "-s":
+            run_clocking(arg)
+            autostart = True
+        
+        
+        print ("auto start:", autostart, "timestamp:", take_timestamp, "argv:", argv, "opts:", opts)
+        
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
 #btn.place(x=50, y=50)
 top.mainloop()
+
+
---
+++ src/timeCounterRn.bat
@@ -0,0 +1,1 @@
+python -m gui.gui_main -s Autostart
